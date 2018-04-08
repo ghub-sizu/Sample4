79357f42a108e1dfe615d17fd4d1866b2749ab34
Shizuo Yoshida <ms-a-sizu@live.jp>
2018-04-08 22:59:10 +0900

    2

---
+++ test/test1.ts
@@ -0,0 +1,12 @@
+import assert = require('assert');
+
+describe("Test Uuite 1", () => {
+    it("Test A", () => {
+        assert.ok(true, "This shouldn't fail");
+    });
+
+    it("Test B", () => {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    });
+});
