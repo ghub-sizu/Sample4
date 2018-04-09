705f088f1ad6393cd19e5c844aeea0daa3d26330
Shizuo Yoshida <ms-a-sizu@live.jp>
2018-04-09 20:43:18 +0900

    test3

--- app-talk.ts
+++ app-talk.ts
@@ -1,11 +1,21 @@
 export namespace App {
     export class Talk {
         public static GetGreeting( now : Date ) : string {
-            if( now.getHours() < 12 ) {
-                return "Hello!";
+            let h = now.getHours();
+            if( h < 5 ) {
+                return "Welcome back!";
+            }
+            else if( h < 12 ) {
+                return "Good morning!";
+            }
+            else if( h < 16 ) {
+                return "Good afternoon!";
+            }
+            else if( h < 22 ) {
+                return "Good evening!";
             }
             else {
-                return "Good evening!";
+                return "Welcome back!";
             }
         }
     }
--- test/test1.js
+++ test/test1.js
@@ -4,8 +4,16 @@ var assert = require("assert");
 var appTalk = require("../app-talk");
 describe("App.Talk", function () {
     it("GetGreeting Test", function () {
-        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 1, 0, 0, 0)), "Hello!");
-        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 13, 0, 0, 0)), "Good evening!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 0, 0, 0, 0)), "Welcome back!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 4, 59, 59, 999)), "Welcome back!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 5, 0, 0, 0)), "Good morning!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 11, 59, 59, 999)), "Good morning!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 12, 0, 0, 0)), "Good afternoon!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 15, 59, 59, 999)), "Good afternoon!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 16, 0, 0, 0)), "Good evening!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 21, 59, 59, 999)), "Good evening!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 22, 0, 0, 0)), "Welcome back!");
+        assert.equal(appTalk.App.Talk.GetGreeting(new Date(2018, 1, 1, 23, 59, 59, 999)), "Welcome back!");
     });
 });
 // describe("Test Uuite 1", () => {
